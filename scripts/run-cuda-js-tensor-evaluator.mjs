ea4a3d877516e016187baf04f5af7a79fcfdc2c9
iteathen <josh.oshiro@gmail.com>
2026-09-08 07:33:45 -0700

    Run Tensor resource layout binding conformance

--- scripts/run-cuda-js-tensor-evaluator.mjs
+++ scripts/run-cuda-js-tensor-evaluator.mjs
@@ -1,3 +1,4 @@
 import '../conformance/cuda-js-tensor-evaluator/run.mjs';
 import '../conformance/cuda-js-tensor-evaluator/runtime.mjs';
 import '../conformance/cuda-js-tensor-evaluator/program-binding.mjs';
+import '../conformance/cuda-js-tensor-evaluator/resource-binding.mjs';
